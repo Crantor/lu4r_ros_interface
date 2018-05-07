f44ca6a8339eaa4f9ef2490f2916a62f92b7e568
Crantor <jdlsicilia@gmail.com>
2018-05-07 11:31:03 +0200

    Robot is moving!

--- scripts/simlab_interpreter.py
+++ scripts/simlab_interpreter.py
@@ -54,37 +54,91 @@ possible_actions = ['ARRIVING',                	# 0
 
 command_list = []
 goals = [
-	["initial","start","beginning"],
-	["kitchen","knife","oven","spoon","fork"],
-	["bathroom","toilet","bath","sink","toothbrush",],
-	['exit','door','escape']]
+	["Goal0","initial","start","beginning"],
+	["Goal1","kitchen","knife","oven","spoon","fork"],
+	["Goal2","bathroom","toilet","bath","sink","toothbrush"],
+	["Goal3","exit","door","escape"]]
 
-#BRINGING(beneficiary:"me",theme:"the book")
+
+#current_action = ""
+isWorking = False
 
 def interpretercallback(data):
+	global isWorking
+
+	def getContent():
+		return data.data.split('(')[1].split(')')[0]
+
 	print 'Received: ' + data.data
 	action = data.data.split('(')[0]
-	content = data.data.split('(')[1].split(')')[0]
+	# content = data.data.split('(')[1].split(')')[0]
+
+	# BRINGING
+	# Example of transcription received: BRINGING(beneficiary:"me",theme:"the book")
+	# if action == possible_actions[4]:
+
+    # CHANGE_OPERATIONAL_STATE (STOP)
+	if action == possible_actions[6]:
+		rospy.wait_for_service('/rosarnl_node/stop')
+		try:
+			stop = rospy.ServiceProxy('/rosarnl_node', stop)
+			stop()
+
+		except rospy.ServiceException, e:
+			print "Service call failed: %s"%e
 
 	# MOTION
+	# Example of transcription received: MOTION(goal:"to the bathroom")
 	if action == possible_actions[13]:
+		# Getting whole content
+		content = getContent()
+		content = content.split(":")[1]
+		content = content.replace("\"","")
+		# Splitting content into words
+		content = content.split(" ")
 
+		# Checking if any of the words is a goal
+		for word in content:
+			index = checkList(word, goals)
 
-	#predicates = xdg.find_predicates(response.text)
-	# connection.send(predicates+'\r\n')
-	#print predicates
+			if(index != -1):
+				goal = goals[index][0]
+				# Giving order to go to the indicated goal if there are no other orders yet.
+				if not isWorking:
+					print("MOTION: Going to ",goal)
+					command_list.append(goal)
+			else:
+				print("I did not understand you, please repeat.")
+
+	if not isWorking and len(command_list) is not 0:
+		isWorking = True
+		# Sending work to the arnl node
+		pub.publish(command_list.pop()) # Getting First command.
 
 def stateCallback(data):
-	pass
+	print "State: " + data.data
+	print("Command_List:")
+	print(command_list)
+	if(data.data) == "REACHED_GOAL":
+		if len(command_list) == 0:
+			isWorking = False
+			print "Aqui llego"
+		else:
+			pub.publish(command_list.pop())
 
 def checkList(x,l):
+	for elem in l:
+		if x in elem:
+			return l.index(elem)
+	return -1
+'''
 	for elem in l:
 		if elem == x:
 			return True
 		if isinstance(elem,list):
 			return checkList(x,elem)
 	return False
-
+'''
 
 def listener():
 	global sub_interpreter
