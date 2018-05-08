3e9df3f5cadee0386b434a2c9c77c5bc853bf65d
elssbbboy <elssbbboy@gmail.com>
2018-05-08 18:38:37 +0200

    BRINGING implemented

--- scripts/simlab_interpreter.py
+++ scripts/simlab_interpreter.py
@@ -1,30 +1,13 @@
 #!/usr/bin/env python
 # encoding=utf8
 
-# encoding=utf8
-
 import rospy
 from std_msgs.msg import String
-import socket
 import sys
-import requests
-import rospkg
-import json
-from math import radians, sin, cos
-from geometry_msgs.msg import Pose2D
-from geometry_msgs.msg import Twist
-import xmltodict
-import xdg_extract as xdg
-import netifaces as ni
 import os
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-
-goal = Pose2D()
-HEADERS = {'content-type': 'application/json'}
-rospack = rospkg.RosPack()
-directory = rospack.get_path('lu4r_ros_interface')
 
 #Subscriber
 sub_interpreter = None
@@ -56,26 +39,27 @@ possible_actions = ['ARRIVING',                	# 0
 command_list = []
 goals = [
 	["Goal0","initial","start","beginning"],
-	["Goal1","kitchen","knife","oven","spoon","fork"],
-	["Goal2","bathroom","toilet","bath","sink","toothbrush"],
+	["Goal1","kitchen"],
+	["Goal2","bathroom","toilet","bath","sink"],
 	["Goal3","exit","door","escape"]]
 
 objects = [
-	["Goal0"],
+	["Goal0","book"],
 	["Goal1","knife","oven","spoon","fork","food"],
-	["Goal2","toothbrush","soap","kraken"],
-	["Goal3","kraken"]]
+	["Goal2","toothbrush","soap"],
+	["Goal3","mobile"]]
 
-#current_action = ""
 isWorking = False
-current_target = ""
-last_target = ""
+current_target = ("","")
+last_target = ("","")
+objects_to_be_taken = []
 
 def interpretercallback(data):
 	global isWorking
 	global current_target
 	global last_target
 	global command_list
+	global objects_to_be_taken
 
 	def getContent():
 		return data.data.split('(')[1].split(')')[0]
@@ -86,13 +70,50 @@ def interpretercallback(data):
 
 	# BRINGING
 	# Example of transcription received: BRINGING(beneficiary:"me",theme:"the book")
-	# if action == possible_actions[4]:
+	if action == possible_actions[4]:
+		# Getting whole content
+		content = getContent()
+		content = content.split("theme:")[1]
+		content = content.replace("\"","")
+		# Splitting content into words
+		content = content.split(" ")
+
+		# Checking if any of the words is an object
+		for word in content:
+			index = checkList(word, objects)
+			if index != -1:
+				objects_to_be_taken.append(word)
+				break
+
+		if index != -1:
+			goal = goals[index][0]
+
+			# Giving order to go to the indicated goal
+			print("BRINGING: Bring the " + objects_to_be_taken[len(objects_to_be_taken)-1])
+
+			if last_target == ("",""):
+				last_target = ("BRINGING1","start")
+
+			if len(command_list) > 0:
+				return_object_to = command_list[len(command_list)-1][1]
+			elif current_target == ("",""):
+				return_object_to = last_target[1]
+			else:
+				return_object_to = current_target[1]
+
+			command_list.append((action+"1", goal))
+			command_list.append((action+"2", return_object_to))
+
+		else:
+			print("I did not understand you, please repeat.")
 
     # CHANGE_OPERATIONAL_STATE (STOP)
 	if action == possible_actions[6]:
 		command_list = []
 		isWorking = False
-		current_target = ""
+		last_target = current_target
+		current_target = ("","")
+		objects_to_be_taken = []
 		os.system('rosservice call /rosarnl_node/stop')
 
 	# MOTION
@@ -108,13 +129,14 @@ def interpretercallback(data):
 		# Checking if any of the words is a goal
 		for word in content:
 			index = checkList(word, goals)
+			if index != -1:
+				break
 
-		if(index != -1):
+		if index != -1:
 			goal = goals[index][0]
-			# Giving order to go to the indicated goal if there are no other orders yet.
-			#if not isWorking:
+			# Giving order to go to the indicated goal
 			print("MOTION: Going to ",goal)
-			command_list.append(goal)
+			command_list.append((action, goal))
 
 		else:
 			print("I did not understand you, please repeat.")
@@ -128,22 +150,29 @@ def interpretercallback(data):
 		# Sending work to the arnl node
 		current_target = command_list.pop(0)
 		last_target = current_target
-		pub.publish(current_target) # Getting First command.
+		pub.publish(current_target[1]) # Getting First command.
 
 def stateCallback(data):
 	global isWorking
 	global current_target
 	global last_target
 	global command_list
+	global objects_to_be_taken
 
 	if(data.data) == "REACHED_GOAL":
+
 		last_target = current_target
+
 		if len(command_list) == 0:
 			isWorking = False
-			current_target = ""
+			current_target = ("","")
 		else:
+
+			if (current_target[0] == "BRINGING1"):
+				print("Give me the " + objects_to_be_taken.pop(0))
+
 			current_target = command_list.pop(0)
-			pub.publish(current_target)
+			pub.publish(current_target[1])
 
 def checkList(x,l):
 	for elem in l:
